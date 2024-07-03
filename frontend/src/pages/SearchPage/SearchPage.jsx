5ba8a4e2f68f322f150d8aa7b4b2c29d1a24976d
Tuan Anh Le <tuanlattnlhp@gmail.com>
2024-07-03 15:07:23 -0400

    Allow user to search a movie based on typed query

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -1,7 +1,8 @@
 import './SearchPage.css'
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
+import {Link} from 'react-router-dom'
 
 function SearchPage(){
     const useQuery = () => {
@@ -9,10 +10,30 @@ function SearchPage(){
     };
 
     const query = useQuery().get('query');
+    const [movies, setMovies] = useState([]);
+    
+    useEffect(() => {
+        if (query) {
+            fetch(`http://localhost:3000/search?query=${query}`)
+                .then(response => response.json())
+                .then(data => {setMovies(data);})
+                .catch(error => console.error('Error fetching search results:', error));
+        }
+    }, [query]);
+
     
     return (
-        <div>
-            <h1>Search Results for {query} </h1>
+        <div className='titlecards'>
+            <div className='card-list-search'>
+                {movies.map((movie,index) => (
+                    <div key={movie.id}>
+                        <Link to={`/moviedetail/${movie.id}`} className='card' key={index}>
+                            <img src={'https://image.tmdb.org/t/p/w500'+movie.backdrop_path} alt={movie.name} />
+                            <p>{movie.original_title}</p>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 
