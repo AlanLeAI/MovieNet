ded273e515dae8ebdfa3a4dc4ad74d4c4fecf48d
Duy Kha <duykha121314888@gmail.com>
2024-07-06 04:49:09 -0500

    Config Prettier

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,11 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { createUserWithEmailAndPassword, 
-    getAuth, 
-    signInWithEmailAndPassword, 
-    signOut} from "firebase/auth"
-import {addDoc, collection, getFirestore, serverTimestamp} from "firebase/firestore"
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  serverTimestamp,
+} from "firebase/firestore";
 import { toast } from "react-toastify";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,7 +27,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -28,41 +35,38 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 // Initialize Authentication
-const auth = getAuth(app)
-const db = getFirestore(app)
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-async function signup(name, email, password){
-    try {
-        const res = await createUserWithEmailAndPassword(auth, email, password)
-        const user = res.user
-        await addDoc(collection(db, "user"), {
-            uid: user.uid,
-            name,
-            authProvider: "local",
-            email,
-            avatar: '../public/imgs/avatar.png',
-            createTime: serverTimestamp()
-        })
-    }
-    catch (error) {
-        console.log(error)
-        toast.error(error.code.split('/')[1].split('-').join(" "))
-    }
+async function signup(name, email, password) {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+      avatar: "../public/imgs/avatar.png",
+      createTime: serverTimestamp(),
+    });
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
 }
 
-async function login(email, password){
-    try {
-        await signInWithEmailAndPassword(auth, email, password)
-    }
-    catch (error) {
-        console.log(error)
-        toast.error(error.code.split('/')[1].split('-').join(" "))
-    }
+async function login(email, password) {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
 }
 
-function logout(){
-    signOut(auth)
+function logout() {
+  signOut(auth);
 }
 
-export {app, auth, db, login, signup, logout}
-
+export { app, auth, db, login, signup, logout };
