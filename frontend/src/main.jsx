5bc99beab379d341d8031c06d0253e8bc8fa043a
Nhat Le <nhatle@Nhats-Laptop.local>
2024-07-08 22:00:56 -0500

    navbar UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,10 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <div>
-        <App />
-        <div>hello</div>
-      </div>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
